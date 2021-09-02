05e9c78df32c75b52cd00cf67d464bbc260ba3fb
Jay the Code Monkey <jchiarulli@protonmail.com>
2021-09-02 07:15:20 -0400

    Added a new sidebar menu icon and the sidebar cross icon is now shown when the sidebar is shown

---
+++ docs/.vuepress/theme/components/SidebarButton.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="sidebar-button" @click="toggleSidebar">
+    <vp-icon v-show="!showSideBar" class="icon" name="menu"></vp-icon>
+    <vp-icon v-show="showSideBar" class="icon" name="cross"></vp-icon>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SidebarButton',
+
+  data: function() {
+    return {
+      showSideBar: false
+    };
+  },
+  methods: {
+    toggleSidebar() {
+      this.showSideBar = !this.showSideBar;
+      this.$emit('toggle-sidebar');
+    }
+  }
+};
+</script>
+
+<style lang="stylus">
+.sidebar-button
+  cursor pointer
+  display none
+  width 1.25rem
+  height 1.25rem
+  position absolute
+  padding 0.6rem
+  top 0.6rem
+  left 1rem
+  text-align center
+  .icon
+    display block
+    width 1.5rem
+    height 1.5rem
+
+@media (max-width: $MQMobile)
+  .sidebar-button
+    display block
+</style>
