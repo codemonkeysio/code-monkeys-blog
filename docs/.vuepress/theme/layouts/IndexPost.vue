0892e5df83686c799e27d5f6dc8efe180cc65672
Jay the Code Monkey <jchiarulli@protonmail.com>
2021-09-11 19:59:24 -0400

    Adjusted footer padding

--- docs/.vuepress/theme/layouts/IndexPost.vue
+++ docs/.vuepress/theme/layouts/IndexPost.vue
@@ -77,37 +77,38 @@ export default {
 h2
   margin: 1.875rem 0
 
-.post-card
-  display: flex
-  align-items: center
-  margin-top: 3.5rem
-  padding: 0 2rem
-  border: 0.125rem solid #16181d
-  border-radius: 1.875rem
-  box-shadow: 0 0.5rem 1rem 0 #16181d
-  transition: 0.2s
-  background-image: radial-gradient(circle at center center, #2c303a, #1f2229)
-  cursor: pointer
-  .post-title-and-summary
-    flex: 8.5
-  .post-pic
-    flex: 1.5
-    padding-top: 1.125rem
-    padding-left: 1.125rem
-    padding-bottom: 1.125rem
-    transition: transform 0.2s;
-.post-card:hover
-  box-shadow: 0.125rem 0.5rem 1rem 0.125rem #0b0c0f
-  .post-pic
-    transform: scale(1.1);
+.theme-default-content
+  padding-bottom: 0
+  .post-card
+    display: flex
+    align-items: center
+    margin-top: 3.5rem
+    padding: 0 2rem
+    border: 0.125rem solid #16181d
+    border-radius: 1.875rem
+    box-shadow: 0 0.5rem 1rem 0 #16181d
+    transition: 0.2s
+    background-image: radial-gradient(circle at center center, #2c303a, #1f2229)
+    cursor: pointer
+    .post-title-and-summary
+      flex: 8.5
+    .post-pic
+      flex: 1.5
+      padding-top: 1.125rem
+      padding-left: 1.125rem
+      padding-bottom: 1.125rem
+      transition: transform 0.2s;
+  .post-card:hover
+    box-shadow: 0.125rem 0.5rem 1rem 0.125rem #0b0c0f
+    .post-pic
+      transform: scale(1.1);
 
-#pagination
-  display: flex
-  justify-content: space-between
-  padding-top: 2.719rem
+  #pagination
+    display: flex
+    justify-content: space-between
+    padding-top: 2rem
 
 @media (max-width: 54.6875rem)
-  h1
   .post-card
     flex-direction: column-reverse
     h2
--- docs/.vuepress/theme/layouts/Topics.vue
+++ docs/.vuepress/theme/layouts/Topics.vue
@@ -65,29 +65,31 @@ export default {
 h2
   margin: 1.875rem 0
 
-.topic-card
-  display: flex
-  align-items: center
-  border: 0.125rem solid #16181d
-  box-shadow: 0 0.5rem 1rem 0 #16181d
-  transition: 0.2s
-  border-radius: 1.875rem
-  margin-top: 3.5rem
-  padding: 0 2rem
-  background-image: radial-gradient(circle at center center, #2c303a, #1f2229)
-  cursor: pointer
-  .title-and-summary
-    flex: 8.5
-  .logo
-    flex: 1.5
-    padding-top: 1.125rem
-    padding-left: 1.125rem
-    padding-bottom: 1.125rem
-    transition: transform 0.2s
-.topic-card:hover
-  box-shadow: 0.125rem 0.5rem 1rem 0.125rem #0b0c0f
-  .logo
-    transform: scale(1.1)
+.theme-default-content
+  padding-bottom: 0
+  .topic-card
+    display: flex
+    align-items: center
+    border: 0.125rem solid #16181d
+    box-shadow: 0 0.5rem 1rem 0 #16181d
+    transition: 0.2s
+    border-radius: 1.875rem
+    margin-top: 3.5rem
+    padding: 0 2rem
+    background-image: radial-gradient(circle at center center, #2c303a, #1f2229)
+    cursor: pointer
+    .title-and-summary
+      flex: 8.5
+    .logo
+      flex: 1.5
+      padding-top: 1.125rem
+      padding-left: 1.125rem
+      padding-bottom: 1.125rem
+      transition: transform 0.2s
+  .topic-card:hover
+    box-shadow: 0.125rem 0.5rem 1rem 0.125rem #0b0c0f
+    .logo
+      transform: scale(1.1)
 
 @media (max-width: 54.6875rem)
   .topic-card
