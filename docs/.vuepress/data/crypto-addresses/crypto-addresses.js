2a370c91b3968e27b700661a64edeff020e3281d
Jay the Code Monkey <jchiarulli@pm.me>
2022-07-19 22:22:20 -0400

    Removed crypto addresses

--- docs/.vuepress/data/crypto-addresses/crypto-addresses.js
+++ docs/.vuepress/data/crypto-addresses/crypto-addresses.js
@@ -6,22 +6,6 @@ export default {
       address: 'bc1q78he85fe63krgsjn6sayy7pna4aeg2ylge2jcj'
     },
     {
-      name: 'Ethereum',
-      abbr: 'ETH',
-      address: '0xe3549C9D5e3E5BBEA2E1ed335ab544AaF895534F'
-    },
-    {
-      name: 'Cardano',
-      abbr: 'ADA',
-      address:
-        'addr1qy208hcc79x9nmw39jleuslxy76x0a0sq4dspyvjnfapy6xdxtzz8wda5j6r9ekv38axvt7qe3d3tkyjt6a803c2cg3qluqus3'
-    },
-    {
-      name: 'Polkadot',
-      abbr: 'DOT',
-      address: '155fxfhvAdFK8KDWUiTnR41YmYaNrbsCPv9h5j3uaKVPqe9e'
-    },
-    {
       name: 'Monero',
       abbr: 'XMR',
       address:
@@ -38,11 +22,6 @@ export default {
       abbr: 'DERO',
       address:
         'dERoSuaxKdgG6ZYuaw3dJyK3P9cZDFDXb3Ssjqt7fyXEPCBtofw1VMfEw5juDCyDQMG6sjhqSGNszjaiCtSAgikc9QSgDRdcow'
-    },
-    {
-      name: 'Dogecoin',
-      abbr: 'DOGE',
-      address: 'DBE6pQQspZ5CsM3UZMJzhHJUJ8kcREwoce'
     }
   ]
 };
