4ba20855e251ec48de68bccd0d652358a273f3df
Jay the Code Monkey <jchiarulli@pm.me>
2023-03-16 11:04:40 -0400

    removing arrr crypto address

--- docs/.vuepress/data/crypto-addresses/crypto-addresses.js
+++ docs/.vuepress/data/crypto-addresses/crypto-addresses.js
@@ -14,14 +14,6 @@ export default {
         '8A34PKqqsm5WhjomT2QBzTgtiECqWs2MSZ2vw8RQQMmDiZAtFuTM94AQb2MmM2cUu2BbkKURrUreuLdMVc84vbqWJQ2GGw6',
       logo: 'donate/logos/monero-donate-logo.png',
       alt: 'Monero Donate Logo'
-    },
-    {
-      name: 'Pirate Chain',
-      abbr: 'ARRR',
-      address:
-        'zs1335vuwfnznefrp8llhfsnkldjg4h02d0gmt9s5xwpgq6y3h6fqtv2zr0qvv69e0rjuur5juap3d',
-      logo: 'donate/logos/pirate-chain-donate-logo.png',
-      alt: 'Pirate Chain Donate Logo'
     }
   ]
-};
+}
