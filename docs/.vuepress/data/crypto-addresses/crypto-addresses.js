713f9e03ebaab4576e3216aa557b491c380bf392
Jay the Code Monkey <jchiarulli@pm.me>
2023-03-20 11:32:32 -0400

    removing xmr address

--- docs/.vuepress/data/crypto-addresses/crypto-addresses.js
+++ docs/.vuepress/data/crypto-addresses/crypto-addresses.js
@@ -7,13 +7,5 @@ export default {
       logo: 'donate/logos/bitcoin-donate-logo.png',
       alt: 'Bitcoin Donate Logo'
     },
-    {
-      name: 'Monero',
-      abbr: 'XMR',
-      address:
-        '8A34PKqqsm5WhjomT2QBzTgtiECqWs2MSZ2vw8RQQMmDiZAtFuTM94AQb2MmM2cUu2BbkKURrUreuLdMVc84vbqWJQ2GGw6',
-      logo: 'donate/logos/monero-donate-logo.png',
-      alt: 'Monero Donate Logo'
-    }
   ]
 }
